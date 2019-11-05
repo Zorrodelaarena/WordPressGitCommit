876eedf82c83bef376013641dfbb60e21068e756
Ryan Cramer <ryan.cramer@gmail.com>
2019-11-05 07:34:26 -0500

    Adding initial code

---
+++ auto-git-commit.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: AutoGitCommit
+Plugin URI: https://github.com/Zorrodelaarena/auto-git-commit/
+Description: Automatically commits and pushes changes made via the wp-admin panel to a git repo
+Version: 0.0.1
+Author: Ryan Cramer
+Author URI: https://secularcoding.com/
+*/
+
+class AutoGitCommit {
+	public static function init() {
+		add_action('upgrader_process_complete', __CLASS__ . '::UpgraderProcessComplete', 10, 2);
+		add_action('_core_updated_successfully', __CLASS__ . '::CoreUpdatedSuccessfully');
+		add_action('activated_plugin', __CLASS__ . '::ActivatedPlugin');
+		add_filter('wp_generate_attachment_metadata', __CLASS__ . '::GenerateAttachmentMetadata');
+	}
+
+	public static function UpgraderProcessComplete($updater, $options) {
+		$message = __CLASS__ . ' by ' . $options['type'] . ' ' . $options['action'];
+		if (isset($options['plugins']) && is_array($options['plugins']) && !empty($options['plugins'])) {
+			$message .= ' on ' . implode(' and ', array_map(function($path) {
+					return array_shift(explode('/', $path));
+				}, $options['plugins']));
+		}
+		self::CommitWithMessage($message);
+	}
+	
+	public static function ActivatedPlugin($plugin) {
+		self::CommitWithMessage('Automatic by activation of ' . $plugin);
+	}
+	
+	public static function CoreUpdatedSuccessfully($wpVersion) {
+		self::CommitWithMessage('Automatic upgrade to WordPress version ' . $wpVersion);
+	}
+	
+	public static function GenerateAttachmentMetadata($metadata) {
+		self::CommitWithMessage('Automatic by new media uploaded');
+		return $metadata;
+	}
+	
+	private static function CommitWithMessage($message) {
+		exec('git add -A && git commit -m ' . escapeshellarg($message) . ' && git push');
+	}
+}
+
+AutoGitCommit::init();
